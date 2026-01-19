27fd31f25359d1ebbe1c91fb0dec0b938d3c7ffb
Adnan_on_Mac <sikadnan2@gmail.com>
2026-01-20 01:40:16 +0530

    Changed cors for production

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173","https://mangobrains.com","https://www.mangobrains.com"],  # Vite dev URL
-    allow_credentials=True,
+    allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
 )
